d7292d09417978a86356c702fd9cdb4c78b40464
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-02-23 16:41:59 -0800

    feat: convert a 10-base number to any base number

--- encode_decode/helper.py
+++ encode_decode/helper.py
@@ -6,6 +6,8 @@
 #    y = divisor
 #    a = quotient
 #    z = coefficient
+
+#conversion from 10 base to binary
 def dec_to_binary(dec_num):
     #ending binary number
     binary_num = ''
@@ -15,13 +17,36 @@ def dec_to_binary(dec_num):
         binary_num += str(dec_num % 2)
         #dec_num becomes my new quotient that is now going to be divided by 2
         dec_num = dec_num // 2
-    
+    #my final result is the last dec_num gotten from the loop + concatenation of the
+    #other coefficients from binary_num:
     final_binary_num = str(dec_num) + binary_num
     return final_binary_num
 
+    #conversion from 10-base to any base (numerical)
+def n_base_to_change(decimal_num, base):
+    #ending base number converted
+    conversion = ''
+    # my division ends when the dividend is 0
+    while decimal_num // base != 0:
+        #concatenate the coefficient, which is calculated by applying mod to my
+        #decimal number with my base as divisor
+        conversion += str(decimal_num % base)
+        #divide decimal_num in base to get quotient
+        decimal_num = decimal_num // base
+
+    #concatenate last coefficient to already gotten coefficients to return result in new base
+    final_base_num = str(decimal_num) + conversion
+    return final_base_num
+
+    #conversion from 10 base to hexadecimal
+    
+
 
 def main():
-    print(dec_to_binary(93))
+    print(dec_to_binary(decimal_number))
+    decimal_number = int(input("What's the number to be changed?: "))
+    base = int(input("What's the base to be changed to?: "))
+    print(n_base_to_change(decimal_number, base))
 
 if __name__ == "__main__":
     main()
