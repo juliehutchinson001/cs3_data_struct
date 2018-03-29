038e84349dcd8f6fe2f500db343944aaeb1c7402
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-29 16:57:33 -0700

    docs: comment of the remove method in sets

--- sets/sets.py
+++ sets/sets.py
@@ -26,10 +26,4 @@ class Set(object):
 
     def remove(self, element):
         #this method removes element from set if exists
-        if self.contains(element):
-            self.size -= 1
-            self.set.delete(element)
-        else: 
-            return ValueError("element to be removed doesn't exists")
-
-    
+        
