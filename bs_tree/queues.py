4a08e9f1b21479c2fbf6779acc09ba956b1341e5
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-13 23:21:49 -0700

    feat: initialize linked queue class

--- bs_tree/queues.py
+++ bs_tree/queues.py
@@ -3,3 +3,7 @@
 from linked_list import LinkedList
 
 
+# Implement LinkedQueue below, then change the assignment at the bottom
+# to use this Queue implementation to verify it passes all tests
+class LinkedQueue(object):
+
