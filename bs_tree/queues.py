b498d6066de42a137c156f9f73930fa2854b946e
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-13 23:49:19 -0700

    feat: implement comments to is_empty queue method

--- bs_tree/queues.py
+++ bs_tree/queues.py
@@ -20,3 +20,5 @@ class LinkedQueue(object):
         return 'Queue({} items, front={})'.format(self.length(), self.front())
 
     def is_empty(self):
+        """Return True if this queue is empty, or False otherwise."""
+        # Check if empty
