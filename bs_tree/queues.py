9e7c5b4363112b37e9f82f5085e6e5ed89a0c8a5
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-13 23:22:58 -0700

    feat: representation of the init method queue class

--- bs_tree/queues.py
+++ bs_tree/queues.py
@@ -7,3 +7,11 @@ from linked_list import LinkedList
 # to use this Queue implementation to verify it passes all tests
 class LinkedQueue(object):
 
+    def __init__(self, iterable=None):
+        """Initialize this queue and enqueue the given items, if any."""
+
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.enqueue(item)
+
